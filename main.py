b8bad0ca15f8c3c6a52f19bd31fbbd52a8db8868
SirFuzzyWalls <alexzandermcgraw@gmail.com>
2025-01-02 15:45:32 -0800

    add basic character counter

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ def main():
     print(text)
     num_words = count_words(text)
     print(f"there are {num_words} words!")
+    char_count = count_characters(text)
+    print(char_count)
 
 def get_book_text(path):
     with open(path) as f:
@@ -11,5 +13,18 @@ def get_book_text(path):
 
 def count_words(text):
     return len(text.split())
-   
+
+def count_characters(text):
+    lowercase_text = text.lower()
+    characters = {}
+
+    for char in lowercase_text:
+        if char in characters:
+            #increment the counter integer by 1
+            characters[char] += 1
+        else: 
+            #add the char to the characters dictionary as the key
+            #then set the counter to 1
+            characters[char] = 1
+    return characters
 main()
