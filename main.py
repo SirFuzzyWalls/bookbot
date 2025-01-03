6eca3a0d9b07eb52d76bdbdcf81b77ee0ef29e2b
SirFuzzyWalls <alexzandermcgraw@gmail.com>
2025-01-02 16:38:39 -0800

    add basic report and ordering of character counter

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
     char_count = count_characters(text)
     print(char_count)
 
+    generate_report(book_path, text)
+
 def get_book_text(path):
     with open(path) as f:
         return f.read()
@@ -20,11 +22,35 @@ def count_characters(text):
 
     for char in lowercase_text:
         if char in characters:
-            #increment the counter integer by 1
             characters[char] += 1
-        else: 
-            #add the char to the characters dictionary as the key
-            #then set the counter to 1
+        elif char.isalpha(): 
             characters[char] = 1
     return characters
+
+def sort_on(dict):
+    return dict["count"]
+
+def convert_and_sort(characters):
+    ordered = []
+    for char, count in characters.items():
+        ordered.append({
+            "character": char,
+            "count": count
+        })
+    ordered.sort(reverse=True, key=sort_on)
+    return ordered
+
+
+
+def generate_report(path, text):
+    chars_by_count = convert_and_sort(count_characters(text))
+    print(f"--- Begin report of {path} ---")
+    print(f"{count_words(text)} words found in the document")
+    print("\n")
+    
+    #iterate through characters by count and print a nice f string for each
+    for el in chars_by_count:
+        print(f"The '{el['character']}' character was found {el['count']} times")
+    print("--- End report ---")
+
 main()
